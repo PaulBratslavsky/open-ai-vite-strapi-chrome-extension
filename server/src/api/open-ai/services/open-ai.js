5105f12474a106f6ef08e8835fb6e0dbcc7bf631
paul bratslavsky <paulbratslavsky@pauls-Mac-mini.local>
2023-04-10 12:52:26 -0500

    fixing code

--- server/src/api/open-ai/services/open-ai.js
+++ server/src/api/open-ai/services/open-ai.js
@@ -13,7 +13,7 @@ module.exports = ({ strapi }) => ({
   
   async openAiRequest(payload, type = "completion") {
     const openai = configureOpenAi(process.env.OPENAI_API_KEY);
-    const defaultPrompt = `Expand and Transform Following Short Text Samples into Comprehensive Technical Articles. Must include headings, sections, and bullet points. Provide an intro, and a bullet point outline at the beginning discussing topics covered. Paragraphs should be between 5 to 8 sentences and should have 8 to 10 paragraphs. Add additional information and context to fill out the article.  Minimum length 2500 words. Return post in markdown:`;
+    const defaultPrompt = `Expand and Transform Following Short Text Samples into Comprehensive Technical Articles. Must include headings, sections, and bullet points. Provide an intro, and a bullet point outline at the beginning discussing topics covered. Paragraphs should be between 5 to 8 sentences and should have 8 to 10 paragraphs. Add additional information and context to fill out the article. Provide code example when necessary.  Minimum length 2500 words. Return post in markdown:`;
 
     async function createTranscription(payload) {
       console.log('Transcribing audio file...');
